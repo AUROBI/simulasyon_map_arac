a1aef4dee5748cf44aa811a63fb3c7ae88aad3eb
emirhan <emirhan@Ubuntu20Foxy.myguest.virtualbox.org>
2026-05-02 02:44:14 +0300

    Setup script eklendi

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Kurulum başlıyor..."
+
+# Map modellerini kopyala
+mkdir -p ~/.gazebo/models
+mkdir -p ~/.gazebo/worlds
+
+cp -r map_models/siyahlar ~/.gazebo/models/
+cp -r map_models/beyazlar ~/.gazebo/models/
+cp -r map_models/maviler ~/.gazebo/models/
+cp -r map_models/yesil ~/.gazebo/models/
+cp map_worlds/map_world.world ~/.gazebo/worlds/
+
+# Build et
+cd Arac_Ws
+colcon build --packages-select bee1_description
+source install/setup.bash
+
+echo "Kurulum tamamlandı!"
+echo "Çalıştırmak için:"
+echo "ros2 launch bee1_description map_and_bee1.launch.py"
