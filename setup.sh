880be3db8317df1638daec67994c597d616057d9
emirhan <emirhan@Ubuntu20Foxy.myguest.virtualbox.org>
2026-05-06 13:46:31 +0300

    Setup script guncellendi

--- setup.sh
+++ setup.sh
@@ -10,10 +10,19 @@ cp -r map_models/maviler ~/.gazebo/models/
 cp -r map_models/yesil ~/.gazebo/models/
 cp map_worlds/map_world.world ~/.gazebo/worlds/
 
+# Eksik klasörleri oluştur
+mkdir -p Arac_Ws/src/bee1_description/config
+mkdir -p Arac_Ws/src/bee1_description/meshes
+mkdir -p Arac_Ws/src/bee1_description/rviz
+
+# Build et
 cd Arac_Ws
+rm -rf build install log
 colcon build --packages-select bee1_description
 source install/setup.bash
 
 echo "Kurulum tamamlandı!"
 echo "Çalıştırmak için:"
+echo "source /opt/ros/foxy/setup.bash"
+echo "source Arac_Ws/install/setup.bash"
 echo "ros2 launch bee1_description map_and_bee1.launch.py"
